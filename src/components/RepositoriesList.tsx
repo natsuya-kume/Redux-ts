23b122dd2ebc4f047b099156d92635af9b8e0f81
NatsuyaKume <65702108+natsuya-kume@users.noreply.github.com>
2021-01-24 02:55:18 +0900

    Calling an Action Creator

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
+import { useTypeSelector } from "../hooks/useTypeSelector";
+import { useActions } from "../hooks/useActions";
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState("");
+  const { searchRepositories } = useActions();
+  const { data, error, loading } = useTypeSelector(
+    (state) => state.repositories
+  );
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    searchRepositories(term);
   };
   return (
     <div>
@@ -12,6 +19,9 @@ const RepositoriesList: React.FC = () => {
         <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
     </div>
   );
 };
